84c7f5ead1df944060d308c0056effc008e9261d
Kidrah31 <hardik.shukla@mavs.uta.edu>
2021-02-05 21:53:31 -0600

    Adding walkthrough and Flixster part 2 functionality

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,20 +3,35 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
+@Parcel
 
 public class Movie {
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+    String releaseDate;
+    boolean IsAdult;
+    public Movie(){}// empty constructor needed by the Parceler library
+
+
+
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath=jsonObject.getString("backdrop_path");
         posterPath=jsonObject.getString("poster_path");
         title= jsonObject.getString("title");
         overview=jsonObject.getString("overview");
+        rating=jsonObject.getDouble("vote_average");
+        movieId=jsonObject.getInt("id");
+        releaseDate = jsonObject.getString("release_date");
+        IsAdult=jsonObject.getBoolean("adult");
+
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies= new ArrayList<>();
@@ -41,5 +56,20 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public boolean isAdult() {
+        return IsAdult;
     }
 }
